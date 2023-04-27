bfe85c2d4c2249e2b9ab0fb43e9988c8f45b9328
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-04-27 10:08:54 +0800

    remove unused dummy code

--- src/pages/dashboard/navboard.tsx
+++ src/pages/dashboard/navboard.tsx
@@ -1,7 +1,6 @@
 import {
   AppShell,
   Button,
-  Group,
   Navbar,
   Header,
   createStyles,
@@ -10,105 +9,105 @@ import {
 } from "@mantine/core";
 import { useState } from "react";
 
-const useStyles = createStyles((theme) => ({
-  header: {
-    paddingBottom: theme.spacing.md,
-    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
-    }`,
-  },
+// const useStyles = createStyles((theme) => ({
+//   header: {
+//     paddingBottom: theme.spacing.md,
+//     marginBottom: `calc(${theme.spacing.md} * 1.5)`,
+//     borderBottom: `${rem(1)} solid ${
+//       theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+//     }`,
+//   },
 
-  footer: {
-    paddingTop: theme.spacing.md,
-    marginTop: theme.spacing.md,
-    borderTop: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
-    }`,
-  },
+//   footer: {
+//     paddingTop: theme.spacing.md,
+//     marginTop: theme.spacing.md,
+//     borderTop: `${rem(1)} solid ${
+//       theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+//     }`,
+//   },
 
-  link: {
-    ...theme.fn.focusStyles(),
-    display: "flex",
-    alignItems: "center",
-    textDecoration: "none",
-    fontSize: theme.fontSizes.sm,
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[1]
-        : theme.colors.gray[7],
-    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
+//   link: {
+//     ...theme.fn.focusStyles(),
+//     display: "flex",
+//     alignItems: "center",
+//     textDecoration: "none",
+//     fontSize: theme.fontSizes.sm,
+//     color:
+//       theme.colorScheme === "dark"
+//         ? theme.colors.dark[1]
+//         : theme.colors.gray[7],
+//     padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+//     borderRadius: theme.radius.sm,
+//     fontWeight: 500,
 
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+//     "&:hover": {
+//       backgroundColor:
+//         theme.colorScheme === "dark"
+//           ? theme.colors.dark[6]
+//           : theme.colors.gray[0],
+//       color: theme.colorScheme === "dark" ? theme.white : theme.black,
 
-      [`& .${getStylesRef("icon")}`]: {
-        color: theme.colorScheme === "dark" ? theme.white : theme.black,
-      },
-    },
-  },
+//       [`& .${getStylesRef("icon")}`]: {
+//         color: theme.colorScheme === "dark" ? theme.white : theme.black,
+//       },
+//     },
+//   },
 
-  linkIcon: {
-    ref: getStylesRef("icon"),
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[2]
-        : theme.colors.gray[6],
-    marginRight: theme.spacing.sm,
-  },
+//   linkIcon: {
+//     ref: getStylesRef("icon"),
+//     color:
+//       theme.colorScheme === "dark"
+//         ? theme.colors.dark[2]
+//         : theme.colors.gray[6],
+//     marginRight: theme.spacing.sm,
+//   },
 
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
-        .color,
-      [`& .${getStylesRef("icon")}`]: {
-        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
-          .color,
-      },
-    },
-  },
-}));
+//   linkActive: {
+//     "&, &:hover": {
+//       backgroundColor: theme.fn.variant({
+//         variant: "light",
+//         color: theme.primaryColor,
+//       }).background,
+//       color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+//         .color,
+//       [`& .${getStylesRef("icon")}`]: {
+//         color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+//           .color,
+//       },
+//     },
+//   },
+// }));
 
-const data = [
-  { link: "", label: "Notifications" },
-  { link: "", label: "Billing" },
-  { link: "", label: "Security" },
-  { link: "", label: "SSH Keys" },
-  { link: "", label: "Databases" },
-  { link: "", label: "Authentication" },
-  { link: "", label: "Other Settings" },
-];
+// const data = [
+//   { link: "", label: "Notifications" },
+//   { link: "", label: "Billing" },
+//   { link: "", label: "Security" },
+//   { link: "", label: "SSH Keys" },
+//   { link: "", label: "Databases" },
+//   { link: "", label: "Authentication" },
+//   { link: "", label: "Other Settings" },
+// ];
 
 export default function Demo() {
-  const { classes, cx } = useStyles();
-  const [active, setActive] = useState("Billing");
+  // const { classes, cx } = useStyles();
+  // const [active, setActive] = useState("Billing");
 
-  const links = data.map((item) => (
-    <a
-      className={cx(classes.link, {
-        [classes.linkActive]: item.label === active,
-      })}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.label);
-      }}
-    >
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <span>{item.label}</span>
-    </a>
-  ));
+  // const links = data.map((item) => (
+  //   <a
+  //     className={cx(classes.link, {
+  //       [classes.linkActive]: item.label === active,
+  //     })}
+  //     href={item.link}
+  //     key={item.label}
+  //     onClick={(event) => {
+  //       event.preventDefault();
+  //       setActive(item.label);
+  //     }}
+  //   >
+  //     <item.icon className={classes.linkIcon} stroke={1.5} />
+  //     <span>{item.label}</span>
+  //   </a>
+  // ));
 
   // TODO CONVERT THIS TO TAILWIND
   // USE THE SANME COLOR THEORY THO
