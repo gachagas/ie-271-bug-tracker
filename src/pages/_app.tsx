fa91e95d29b32a95adbf5755ae0be8af7f74f915
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-04-27 10:14:16 +0800

    add mantine provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { MantineProvider, createEmotionCache } from "@mantine/core";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -10,9 +10,21 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const myCache = createEmotionCache({
+    key: "mantine",
+    prepend: false,
+  });
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: "dark" }}
+        emotionCache={myCache}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
