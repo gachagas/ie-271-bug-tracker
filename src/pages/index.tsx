3a0a894f119abec3fa9fad47e2d176ddcf0e9def
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-05-14 23:42:43 +0800

    fix eslint error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -110,7 +110,7 @@ const Home: NextPage = () => {
 export default Home;
 
 const AuthShowcase: React.FC = () => {
-  const { data: sessionData, status: sessionStatus } = useSession();
+  const { data: sessionData } = useSession();
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
