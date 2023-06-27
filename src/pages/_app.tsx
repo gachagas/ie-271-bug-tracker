54c87e7d0a0a1ff52c89ca8b8c499ecf8359e5ae
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-06-27 17:20:42 +0800

    init add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { MantineProvider, createEmotionCache } from "@mantine/core";
 import { api } from "~/utils/api";
+import Script from "next/script";
 
 import "~/styles/globals.css";
 
@@ -16,16 +17,36 @@ const MyApp: AppType<{ session: Session | null }> = ({
   });
 
   return (
-    <SessionProvider session={session}>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{ colorScheme: "dark" }}
-        emotionCache={myCache}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </SessionProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-J697KLK62K"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-J697KLK62K', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <SessionProvider session={session}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ colorScheme: "dark" }}
+          emotionCache={myCache}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </SessionProvider>
+    </>
   );
 };
 
